72e90f2064bee58b49ab5cc3907cf9647a996a50
Vladimir Shmakov <vshmakov@atechnics.ru>
2025-06-02 17:48:36 +0500

    second commit

---
+++ src/main/java/com/example/todolist/config/JpaConfig.java
@@ -0,0 +1,12 @@
+package com.example.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.example.todolist.dao")
+public class JpaConfig {
+
+}
